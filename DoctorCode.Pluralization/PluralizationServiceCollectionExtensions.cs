317a512bbc27ceeef2d494421f65e12fbe0d65a0
Alireza <doctorcode.org@gmail.com>
2019-10-11 12:51:23 +0330

    initial

---
+++ DoctorCode.Pluralization/PluralizationServiceCollectionExtensions.cs
@@ -0,0 +1,14 @@
+﻿using DoctorCode.Pluralization;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    public static class PluralizationServiceCollectionExtensions
+    {
+        public static IServiceCollection AddPluralization(this IServiceCollection services)
+        {
+            services.TryAddSingleton<PluralizationService, EnglishPluralizationService>();
+            return services;
+        }
+    }
+}
---
+++ DoctorCode.Pluralization/StringBidirectionalDictionary.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+
+namespace DoctorCode.Pluralization
+{
+    internal class StringBidirectionalDictionary : BidirectionalDictionary<string, string>
+    {
+        internal StringBidirectionalDictionary()
+            : base()
+        { }
+
+        internal StringBidirectionalDictionary(Dictionary<string, string> firstToSecondDictionary)
+            : base(firstToSecondDictionary)
+        { }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Globalization", "CA1308:NormalizeStringsToUppercase")]
+        internal override bool ExistsInFirst(string value)
+        {
+            return base.ExistsInFirst(value.ToLowerInvariant());
+        }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Globalization", "CA1308:NormalizeStringsToUppercase")]
+        internal override bool ExistsInSecond(string value)
+        {
+            return base.ExistsInSecond(value.ToLowerInvariant());
+        }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Globalization", "CA1308:NormalizeStringsToUppercase")]
+        internal override string GetFirstValue(string value)
+        {
+            return base.GetFirstValue(value.ToLowerInvariant());
+        }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Globalization", "CA1308:NormalizeStringsToUppercase")]
+        internal override string GetSecondValue(string value)
+        {
+            return base.GetSecondValue(value.ToLowerInvariant());
+        }
+
+    }
+}
